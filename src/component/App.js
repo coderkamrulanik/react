d60510ec98bafe03ba7d4e214f21fb5018e8df67
Anik <coderkamrulanik>
2020-01-20 23:48:30 +0600

    added new file

---
+++ src/component/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import First from './First/First'
+import Counter from './Counter/Counter'
+
+function App() {
+
+  let p1= 'Anik'
+  return (
+    <div className="App">
+     
+{/* <First name="Kamrul" email="kamrulanikcse@gmail.com" address="Tangail"></First>
+<First name="Hasan" email="kamrulanik@gmail.com" address="Tangail"></First>
+<First name="Anik" email="beforsure@gmail.com" address="Dhaka"></First> */}
+
+
+<Counter></Counter>
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Counter/Counter.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react'
+
+class Counter extends Component {
+
+
+constructor(props){
+    super(props)
+
+    this.state={
+        count: 10
+    }
+}
+
+decrease =()=>{
+    this.setState({
+        count: this.state.count-1
+    })
+}
+
+increase =()=>{
+    this.setState({
+        count: this.state.count+1
+    })
+}
+
+
+
+render(){
+
+    return(
+
+<div>
+   
+<h1><button onClick={this.decrease}> - </button> {this.state.count} <button onClick={this.increase}> + </button></h1>
+
+</div>
+    ) 
+}
+
+}
+
+export default Counter
